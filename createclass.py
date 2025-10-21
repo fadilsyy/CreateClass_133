e75a9119c31d712d4c1a187ad4cdc5b7084701c6
fadilsyy <fadilasytar@gmail.com>
2025-10-21 13:43:18 +0700

    membuat class PersegiPanjang

---
+++ createclass.py
@@ -0,0 +1,15 @@
+class   PersegiPanjang:
+    def __init__(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
+    
+    def luas(self):
+        return self.panjang * self.lebar
+    
+    def __str__(self):
+        return f"Persegi Panjang, Panjang {self.panjang} cm, dan lebar {self.lebar} cm"
+    
+
